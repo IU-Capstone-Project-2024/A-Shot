c8b6e0b3f56125e2129ba9196d07672f5803cebe
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2024-06-19 19:29:08 +0300

    small fixes in imports

--- desktop/src/main/kotlin/screen/import_/component/Failure.kt
+++ desktop/src/main/kotlin/screen/import_/component/Failure.kt
@@ -1,14 +1,22 @@
 package screen.import_.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun BoxScope.Failure(
