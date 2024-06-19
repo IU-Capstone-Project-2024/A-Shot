38f9d4713f130aaeef1cafde3608c93967a32022
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2024-06-19 19:28:41 +0300

    Add loading screen
    need to fix fonts

--- desktop/src/main/kotlin/screen/import_/component/Loading.kt
+++ desktop/src/main/kotlin/screen/import_/component/Loading.kt
@@ -1,14 +1,16 @@
 package screen.import_.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun BoxScope.Loading(
@@ -16,19 +18,85 @@ fun BoxScope.Loading(
     progress: Float,
     onCancel: () -> Unit,
 ) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .align(Alignment.Center),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .align(Alignment.Center)
     ) {
-        Text(text = "Importing ${dirName}")
-        Spacer(modifier = Modifier.height(40.dp))
-        LinearProgressIndicator(progress = progress)
-        Spacer(modifier = Modifier.height(40.dp))
-        Button(onClick = onCancel) {
-            Text(text = "Cancel")
+        Text(
+            text = "Importing ${dirName}",
+            style = MaterialTheme.typography.button.copy(
+                fontSize = 32.sp
+            ),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 75.dp)
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            CircularProgressIndicator(
+                progress = progress,
+                strokeWidth = 4.dp,
+                modifier = Modifier.size(50.dp),
+                color = Color(0xFF21005D),
+            )
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(100.dp),
+        ) {
+            Column {
+                LinearProgressIndicator(
+                    progress = progress,
+                    color = Color(0xFF21005D),
+                    modifier = Modifier.size(404.dp, 4.dp)
+                )
+                Row {
+                    Spacer(modifier = Modifier.size(50.dp, 10.dp))
+                    Text(
+                        //todo: set how much left
+                        text ="text1",
+                        style = MaterialTheme.typography.button.copy(
+                            fontSize = 13.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.size(250.dp, 10.dp))
+                    Text(
+                        //todo: set how much time left
+                        text = "text2",
+                        style = MaterialTheme.typography.button.copy(
+                            fontSize = 13.sp
+                        ))
+                    Spacer(modifier = Modifier.size(50.dp, 10.dp))
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(30.dp)
+        ) {
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color(0xFFEADDFF),
+                ),
+                onClick = onCancel,
+                modifier = Modifier.size(100.dp, 32.dp),
+                elevation = ButtonDefaults.elevation(0.dp),
+                border = BorderStroke(3.dp, Color(0xFF21005D)),
+                shape = RoundedCornerShape(30.dp),
+            ) {
+                Text(
+                    text = "Cancel",
+                    style = MaterialTheme.typography.button.copy(
+                        fontSize = 13.sp
+                    )
+                    //todo: fix font
+                )
+            }
         }
     }
 }
