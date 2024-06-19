dd9a9d6aeb0fbedaa91de83c92eb56d030ab270e
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2024-06-19 21:57:37 +0300

    fix bugs

--- desktop/src/main/kotlin/screen/import_/component/Loading.kt
+++ desktop/src/main/kotlin/screen/import_/component/Loading.kt
@@ -1,6 +1,5 @@
 package screen.import_.component
 
-import style.Roboto
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
@@ -10,29 +9,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import style.Roboto
 
 @Composable
 fun BoxScope.Loading(
     dirName: String,
     progress: Float,
     onCancel: () -> Unit,
+    mainColor: Color = Color(0xFF21005D)
 ) {
     Box(modifier = Modifier.align(Alignment.TopCenter)) {
         Text(
-        text = "Importing ${dirName}",
-        style = MaterialTheme.typography.body1.copy(
+            text = "Importing $dirName",
+            style = MaterialTheme.typography.body1.copy(
                 letterSpacing = 0.5.sp,
                 fontSize = 32.sp,
                 fontFamily = Roboto,
                 fontWeight = FontWeight.Medium,
                 color = mainColor
-        ),
-        modifier = Modifier
-            .align(Alignment.TopCenter)
-            .padding(top = 108.dp)
-    )
+            ),
+            modifier = Modifier
+                .padding(top = 108.dp)
+        )
     }
     Box(
         modifier = Modifier
@@ -42,7 +43,7 @@ fun BoxScope.Loading(
             progress = progress,
             strokeWidth = 4.dp,
             modifier = Modifier.size(50.dp),
-            color = Color(0xFF21005D),
+            color = mainColor,
         )
     }
     Box(
@@ -53,14 +54,14 @@ fun BoxScope.Loading(
         Column {
             LinearProgressIndicator(
                 progress = progress,
-                color = Color(0xFF21005D),
+                color = mainColor,
                 modifier = Modifier.size(404.dp, 4.dp)
             )
             Row {
                 Spacer(modifier = Modifier.size(45.dp, 16.dp))
                 Text(
                     //todo: set how much left
-                    text ="text1",
+                    text = "text1",
                     style = MaterialTheme.typography.body1.copy(
                         fontSize = 16.sp,
                         fontFamily = Roboto,
@@ -82,6 +83,7 @@ fun BoxScope.Loading(
             }
         }
     }
+
     Box(
         modifier = Modifier
             .align(Alignment.BottomCenter)
@@ -94,12 +96,12 @@ fun BoxScope.Loading(
             onClick = onCancel,
             modifier = Modifier.size(150.dp, 32.dp),
             elevation = ButtonDefaults.elevation(0.dp),
-            border = BorderStroke(3.dp, Color(0xFF21005D)),
+            border = BorderStroke(3.dp, mainColor),
             shape = RoundedCornerShape(30.dp),
         ) {
             Text(
                 text = "Cancel",
-                MaterialTheme.typography.body2.copy(
+                style = MaterialTheme.typography.body2.copy(
                     fontSize = 16.sp,
                     fontFamily = Roboto,
                     fontWeight = FontWeight.Medium,
@@ -107,7 +109,8 @@ fun BoxScope.Loading(
                 )
             )
         }
-    }    
+    }
+
 }
 
 @Composable
@@ -119,7 +122,7 @@ private fun LoadingPreview() {
         Loading(
             dirName = "SomeDir",
             progress = 0.75f,
-            onCancel = {},
+            onCancel = {}
         )
     }
 }
