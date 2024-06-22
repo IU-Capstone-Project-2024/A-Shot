541e86d6f017615f90bae2f500e782db775ea96d
a <artem.myasoedov@gmail.com>
2024-06-22 11:50:47 +0300

    Added folder selector

--- desktop/src/main/kotlin/Main.kt
+++ desktop/src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -25,6 +23,12 @@ import screen.cull.CullScreen
 import screen.cull.CullViewModel
 import screen.import_.ImportScreen
 import screen.overview.OverviewScreen
+import util.AsyncImage
+import java.awt.Dimension
+import java.io.File
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileNameExtensionFilter
+import kotlin.math.absoluteValue
 
 val PrimaryColor = Color(0xFFEDE7F6)
 val ContentColor = Color(0xFF7E57C2)
@@ -107,6 +111,7 @@ fun main() {
 			undecorated = false //Should be true for custom Title Bar
 		) {
 //		App(window = window)
+			val scope = rememberCoroutineScope()
 			val loader = remember { LoadingPipeline() }
 			val good = remember { mutableStateListOf<String>() }
 			val bad = remember { mutableStateListOf<String>() }
@@ -126,29 +131,52 @@ fun main() {
 
 			Column(modifier = Modifier.fillMaxSize()) {
 				Button(onClick = {
-					val path = "/home/a/Projects/A-Shot/utils/src/blur_filtering/samples/"
-					val result = loader.load(path)
-					if (!result) {
-						println("Too many clicks")
+					scope.launch {
+						val fileChooser = JFileChooser().apply {
+							dialogTitle = "Select a file or directory"
+							fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+							fileFilter = FileNameExtensionFilter("Directory", "directory")
+						}
+
+						if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+							val result = loader.load(fileChooser.selectedFile.absolutePath)
+							if (!result) {
+								println("Too many clicks")
+							}
+						}
 					}
 				}) {
 					Text("Load")
 				}
 
-				Row {
-					Column(Modifier.weight(1f)) {
-						Text("Good items")
-						LazyColumn {
-							items(good) { item ->
-								Text(item)
+				Row(modifier = Modifier.weight(1f).fillMaxWidth()) {
+					Column(Modifier.weight(1f).fillMaxHeight()) {
+						Text("Good items: ${good.size}")
+						LazyColumn(modifier = Modifier.weight(1f).fillMaxWidth()) {
+							items(
+								good,
+								key = { item -> item }
+							) { item ->
+								AsyncImage(
+									modifier = Modifier.aspectRatio(1f),
+									file = File(item),
+									dimension = Dimension(1024, 1024)
+								)
 							}
 						}
 					}
-					Column(Modifier.weight(1f)) {
-						Text("Bad items")
-						LazyColumn {
-							items(bad) { item ->
-								Text(item)
+					Column(Modifier.weight(1f).fillMaxHeight()) {
+						Text("Bad items: ${bad.size}")
+						LazyColumn(modifier = Modifier.weight(1f).fillMaxWidth()) {
+							items(
+								bad,
+								key = { item -> item }
+							) { item ->
+								AsyncImage(
+									modifier = Modifier.aspectRatio(1f),
+									file = File(item),
+									dimension = Dimension(1024, 1024)
+								)
 							}
 						}
 					}
