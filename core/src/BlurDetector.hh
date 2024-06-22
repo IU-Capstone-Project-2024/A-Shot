5afb2721857d85f021bb35ee9a6e1beaff27c000
a <artem.myasoedov@gmail.com>
2024-06-22 19:19:40 +0300

    Added ImageMagick library

--- core/src/BlurDetector.hh
+++ core/src/BlurDetector.hh
@@ -24,7 +24,7 @@ using ImageBlur = std::pair<Magick::Image, float>;
 
 class BlurDetector : PipelineStep<Magick::Image, ImageBlur> {
 private:
-	const char *MODEL = "/home/a/Projects/A-Shot/core/checkpoints/D-DFFNet.ort";
+	const char *MODEL = "checkpoints/D-DFFNet.ort";
 
 	static const int INPUT_WIDTH = 320;
 	static const int INPUT_HEIGHT = 320;
